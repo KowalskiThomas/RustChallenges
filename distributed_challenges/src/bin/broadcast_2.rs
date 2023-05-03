9c14389d5e1c35963bc0a371380ec92807014b19
Thomas Kowalski <thomas.kowalski@datadoghq.com>
2023-05-04 00:45:41 +0200

    Slight refactor

--- distributed_challenges/src/bin/broadcast_2.rs
+++ distributed_challenges/src/bin/broadcast_2.rs
@@ -1,11 +1,11 @@
 use std::{
     collections::{HashMap, HashSet},
-    sync::mpsc::Sender,
+    sync::mpsc::{Receiver, Sender},
     thread::sleep,
     time::Duration,
 };
 
-use node_driver::{Body, InputInterface, Maelstrom, Message};
+use node_driver::{Body, InputInterface, Maelstrom, Message, NodeMetadata, OutputInterface};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -57,95 +57,134 @@ fn timer(tx: Sender<Event>) {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    let (mut node_metadata, _, mut output) = Maelstrom::init()?;
-    let (tx, rx) = std::sync::mpsc::channel::<Event>();
+struct Node {
+    all_known: HashSet<usize>,
+    topology: Option<HashMap<String, Vec<String>>>,
 
-    let tx_clone = tx.clone();
-    std::thread::spawn(move || read_stdin(tx_clone));
-    std::thread::spawn(move || timer(tx));
+    rx: Option<Receiver<Event>>,
+    node_metadata: Option<NodeMetadata>,
+    output: Option<OutputInterface>,
+}
 
-    let mut all_known = HashSet::new();
-    let mut my_topology: Option<HashMap<String, Vec<String>>> = None;
+impl Node {
+    fn new() -> Node {
+        Node {
+            all_known: HashSet::new(),
+            topology: None,
 
-    loop {
-        if let Ok(ev) = rx.try_recv() {
-            let mut to_send: Option<Message<BroadcastPayload>> = None;
-            match ev {
-                Event::Eof => break,
-                Event::TimeToGossip => {
-                    eprintln!("Gossiping !");
-                    if let Some(top) = &my_topology {
-                        let my_neighbours = top.get(&node_metadata.node_id);
-                        if let Some(my_neighbours) = my_neighbours {
-                            for node in my_neighbours.iter() {
-                                eprintln!("Sending to {}", node);
-                                output
-                                    .send_msg(Message {
-                                        src: node_metadata.node_id.clone(),
-                                        dst: node.clone(),
-                                        body: Body {
-                                            msg_id: Some(node_metadata.get_next_msg_id()),
-                                            in_reply_to: None,
-                                            payload: BroadcastPayload::Gossip {
-                                                known: all_known.clone(),
+            rx: None,
+            node_metadata: None,
+            output: None,
+        }
+    }
+
+    fn main_loop(&mut self) {
+        let rx = self.rx.as_ref().unwrap();
+        let node_metadata = self.node_metadata.as_mut().unwrap();
+        let output = self.output.as_mut().unwrap();
+
+        loop {
+            if let Ok(ev) = rx.try_recv() {
+                let mut to_send: Option<Message<BroadcastPayload>> = None;
+                match ev {
+                    Event::Eof => break,
+                    Event::TimeToGossip => {
+                        eprintln!("Gossiping !");
+                        if let Some(top) = &self.topology {
+                            let my_neighbours = top.get(&node_metadata.node_id);
+                            if let Some(my_neighbours) = my_neighbours {
+                                for node in my_neighbours.iter() {
+                                    eprintln!("Sending to {}", node);
+                                    output
+                                        .send_msg(Message {
+                                            src: node_metadata.node_id.clone(),
+                                            dst: node.clone(),
+                                            body: Body {
+                                                msg_id: Some(node_metadata.get_next_msg_id()),
+                                                in_reply_to: None,
+                                                payload: BroadcastPayload::Gossip {
+                                                    known: self.all_known.clone(),
+                                                },
                                             },
-                                        },
-                                    })
-                                    .unwrap()
+                                        })
+                                        .unwrap()
+                                }
                             }
                         }
                     }
-                }
-                Event::MessageReceived(msg) => {
-                    match &msg.body.payload {
-                        BroadcastPayload::Read {} => {
-                            eprintln!("Received request to read");
-                            let mut msgs = Vec::new();
-                            for x in all_known.iter() {
-                                msgs.push(*x)
+                    Event::MessageReceived(msg) => {
+                        match &msg.body.payload {
+                            BroadcastPayload::Read {} => {
+                                eprintln!("Received request to read");
+                                let mut msgs = Vec::new();
+                                for x in self.all_known.iter() {
+                                    msgs.push(*x)
+                                }
+
+                                to_send = Some(msg.to_response(
+                                    None,
+                                    BroadcastPayload::ReadOk { messages: msgs },
+                                ));
                             }
+                            BroadcastPayload::Broadcast { message: x } => {
+                                eprintln!("Received broadcast {}", x);
+                                self.all_known.insert(*x);
 
-                            to_send = Some(
-                                msg.to_response(None, BroadcastPayload::ReadOk { messages: msgs }),
-                            );
-                        }
-                        BroadcastPayload::Broadcast { message: x } => {
-                            eprintln!("Received broadcast {}", x);
-                            all_known.insert(*x);
-
-                            to_send = Some(msg.to_response(None, BroadcastPayload::BroadcastOk {}));
-                        }
-                        BroadcastPayload::Topology { topology } => {
-                            eprintln!("Received topology");
-                            my_topology = Some(topology.clone());
-                            to_send = Some(msg.to_response(None, BroadcastPayload::TopologyOk {}));
-                        }
-                        BroadcastPayload::Gossip { known } => {
-                            eprintln!("Received gossiping");
-                            for x in known.iter() {
-                                all_known.insert(*x);
+                                to_send =
+                                    Some(msg.to_response(None, BroadcastPayload::BroadcastOk {}));
+                            }
+                            BroadcastPayload::Topology { topology } => {
+                                eprintln!("Received topology");
+                                self.topology = Some(topology.clone());
+                                to_send =
+                                    Some(msg.to_response(None, BroadcastPayload::TopologyOk {}));
+                            }
+                            BroadcastPayload::Gossip { known } => {
+                                eprintln!("Received gossiping");
+                                for x in known.iter() {
+                                    self.all_known.insert(*x);
+                                }
+                            }
+                            BroadcastPayload::ReadOk { messages: _ } => {
+                                panic!("I don't have that");
+                            }
+                            BroadcastPayload::BroadcastOk {} => {
+                                panic!("I don't have that");
+                            }
+                            BroadcastPayload::TopologyOk {} => {
+                                panic!("I don't have that");
                             }
                         }
-                        BroadcastPayload::ReadOk { messages: _ } => {
-                            panic!("I don't have that");
-                        }
-                        BroadcastPayload::BroadcastOk {} => {
-                            panic!("I don't have that");
-                        }
-                        BroadcastPayload::TopologyOk {} => {
-                            panic!("I don't have that");
-                        }
-                    }
 
-                    if let Some(mut msg) = to_send {
-                        msg.body.msg_id = Some(node_metadata.get_next_msg_id());
-                        output.send_msg(msg).unwrap();
+                        if let Some(mut msg) = to_send {
+                            msg.body.msg_id = Some(node_metadata.get_next_msg_id());
+                            output.send_msg(msg).unwrap();
+                        }
                     }
                 }
             }
         }
     }
 
+    fn start(&mut self) -> anyhow::Result<()> {
+        let (node_metadata, _, output) = Maelstrom::init()?;
+        let (tx, rx) = std::sync::mpsc::channel::<Event>();
+
+        self.rx = Some(rx);
+        self.output = Some(output);
+        self.node_metadata = Some(node_metadata);
+
+        let tx_clone = tx.clone();
+        std::thread::spawn(move || read_stdin(tx_clone));
+        std::thread::spawn(move || timer(tx));
+
+        self.main_loop();
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let mut node = Node::new();
+    node.start()?;
     Ok(())
 }
